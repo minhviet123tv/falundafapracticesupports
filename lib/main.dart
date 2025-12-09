b3726db4192960dd4a75741f9053dae15c0ece1b
minhviet123tv <136576401+minhviet123tv@users.noreply.github.com>
2025-12-09 16:31:23 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'dart:async';
-import 'dart:isolate';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:in_app_update/in_app_update.dart';
 
