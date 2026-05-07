9e90a120d7c00d210976f5c38b9e188146346dd0
minhviet123tv <136576401+minhviet123tv@users.noreply.github.com>
2026-05-07 16:39:22 +0700

    ok

--- lib/menu/play_audio_webview.dart
+++ lib/menu/play_audio_webview.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
 
 import 'package:wakelock_plus/wakelock_plus.dart';
 import 'package:webview_flutter/webview_flutter.dart';
@@ -32,13 +33,14 @@ class _WebViewBrowserAudioState extends State<WebViewBrowserAudio> {
   late final WebViewController _controller; // Bộ điều khiển cho webview
   var textSize16 = TextStyle(fontSize: 16);
   var textSize18 = TextStyle(fontSize: 18, fontWeight: FontWeight.w600);
+  bool _isWakelockActive = false;
 
   //B. Khởi tạo khi mới mở
   @override
   void initState() {
     super.initState();
     // Keep screen awake while this audio webview is open.
-    WakelockPlus.enable();
+    unawaited(_setWakelock(true));
 
     // #docregion platform_features
     late final PlatformWebViewControllerCreationParams params;
@@ -70,6 +72,7 @@ class _WebViewBrowserAudioState extends State<WebViewBrowserAudio> {
           },
           onPageFinished: (String url) {
             debugPrint('Page finished loading: $url');
+            unawaited(_injectPlaybackTrackingScript());
           },
           onWebResourceError: (WebResourceError error) {
             debugPrint('''
@@ -110,6 +113,17 @@ class _WebViewBrowserAudioState extends State<WebViewBrowserAudio> {
           );
         },
       )
+      ..addJavaScriptChannel(
+        'PlaybackState',
+        onMessageReceived: (JavaScriptMessage message) {
+          final state = message.message.toLowerCase();
+          if (state == 'play') {
+            unawaited(_setWakelock(true));
+          } else if (state == 'pause' || state == 'ended') {
+            unawaited(_setWakelock(false));
+          }
+        },
+      )
 
       // Trang web đầu tiên khi mới mở trang
       ..loadRequest(Uri.parse('${widget.linkUrl}')); // https://www.ganjingworld.com/embed/1fdmph5i6al3ExcRdoWQUzS541l51c
@@ -127,8 +141,67 @@ class _WebViewBrowserAudioState extends State<WebViewBrowserAudio> {
 
   @override
   void dispose() {
-    WakelockPlus.disable();
+    unawaited(_pauseAllMedia());
+    unawaited(_setWakelock(false));
     super.dispose();
+  }
+
+  Future<void> _setWakelock(bool enabled) async {
+    if (_isWakelockActive == enabled) return;
+    _isWakelockActive = enabled;
+    if (enabled) {
+      await WakelockPlus.enable();
+    } else {
+      await WakelockPlus.disable();
+    }
+  }
+
+  Future<void> _injectPlaybackTrackingScript() async {
+    await _controller.runJavaScript('''
+      (() => {
+        if (window.__fdPlaybackTrackingInstalled) return;
+        window.__fdPlaybackTrackingInstalled = true;
+
+        const send = (value) => {
+          if (window.PlaybackState && window.PlaybackState.postMessage) {
+            window.PlaybackState.postMessage(value);
+          }
+        };
+
+        const bindMedia = (media) => {
+          if (!media || media.__fdPlaybackBound) return;
+          media.__fdPlaybackBound = true;
+          media.addEventListener('play', () => send('play'));
+          media.addEventListener('playing', () => send('play'));
+          media.addEventListener('pause', () => send('pause'));
+          media.addEventListener('ended', () => send('ended'));
+        };
+
+        const bindAll = () => {
+          document.querySelectorAll('audio, video').forEach(bindMedia);
+        };
+
+        bindAll();
+        const observer = new MutationObserver(() => bindAll());
+        observer.observe(document.documentElement, { childList: true, subtree: true });
+      })();
+    ''');
+  }
+
+  Future<void> _pauseAllMedia() async {
+    try {
+      await _controller.runJavaScript('''
+        (() => {
+          document.querySelectorAll('audio, video').forEach((media) => {
+            if (!media.paused) {
+              media.pause();
+            }
+          });
+        })();
+      ''');
+    } catch (_) {
+      // Ignore errors while page is tearing down.
+    }
   }
 
   //D. Trang
